0b332da66a4c35fcc2259786359c787698ba1845
DmitryZ <dmitriyzagulyaev@gmail.com>
2016-04-28 22:35:36 +0500

    Zagulyaev

--- example3/src/main/java/Decoder.java
+++ example3/src/main/java/Decoder.java
@@ -1,12 +1,10 @@
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.compress.compressors.xz.XZCompressorInputStream;
 import org.apache.commons.compress.compressors.xz.XZCompressorOutputStream;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
-public class Encoder {
+public class Decoder {
 
 /*
 Enter String: *****
@@ -16,15 +14,13 @@ Enter String: *****
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter String: ");
-        String s = br.readLine();
-
-        ByteArrayOutputStream bout = new ByteArrayOutputStream();
-        XZCompressorOutputStream xzOut = new XZCompressorOutputStream(bout);
-        xzOut.write(s.getBytes());
-        xzOut.close();
-
-        String result = Base64.encodeBase64String(bout.toByteArray());
-        System.out.println(result);
+        String str = br.readLine();
+        byte a[] = Base64.decodeBase64(str);
+        byte buf[] = new byte[1024];
+        ByteArrayInputStream bin = new ByteArrayInputStream(a);
+        XZCompressorInputStream xzIn = new XZCompressorInputStream(bin);
+        xzIn.read(buf);
+        System.out.println(new String(buf));
     }
 
 }
